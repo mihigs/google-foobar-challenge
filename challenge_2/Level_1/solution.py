99bfcfa7c3fd28325a799fab4a39e0652e4474d3
unknown <miralem.avdic@mss.ba>
2021-08-21 13:05:03 +0200

    Solution for level 1

--- challenge_2/Level_1/solution.py
+++ challenge_2/Level_1/solution.py
@@ -1,7 +1,24 @@
-def​ solution(i):
-    if i < 0 or i > 10000:
+def solution(i):
+    if(i < 0 or i > 10000):
         return;
-    print(i)
+    primeString = generatePrimeString(10005);
+    return primeString[i:i+5]
 
+def isPrime(n):
+    if(n == 2):
+        return True
+    elif(n < 2 or not n & 1):
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
+        if n % i == 0:
+            return False
+    return True
 
-solution(3) 
+def generatePrimeString(n):
+    primeString = '2'
+    iterator = 1
+    while len(primeString) < n:
+        if(isPrime(iterator)):
+            primeString += str(iterator)
+        iterator += 2
+    return primeString
